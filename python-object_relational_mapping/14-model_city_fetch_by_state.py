1efb223debd1124e8e52a30bb5cfdbbdbd0285db
klysley-des <k.lysley@alustudent.com>
2026-07-28 17:42:04 +0200

    Fix city model and fetch by state

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,24 +1,30 @@
 #!/usr/bin/python3
 """
-Script outputs all City objects from the database hbtn_0e_14_usa
+Script lists all City objects from the database hbtn_0e_14_usa
 """
 
-
 import sys
-from model_state import Base, State
+from model_state import State
 from model_city import City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
-if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        )
+    )
+
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    st_cty = session.query(State, City).filter(State.id == City.state_id).all()
+    results = session.query(State, City).filter(
+        State.id == City.state_id
+    ).order_by(City.id).all()
 
-    for state, city in st_cty:
+    for state, city in results:
         print("{}: ({}) {}".format(state.name, city.id, city.name))
+
+    session.close()
